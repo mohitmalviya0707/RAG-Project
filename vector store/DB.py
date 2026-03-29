36f60cf6e21d172f588b388f28b37f45ecfe4987
Mohit Malviya <mohitmalviya0107@gmail.com>
2026-03-29 19:44:48 +0530

    Add documents to vector store in DB.py

--- vector store/DB.py
+++ vector store/DB.py
@@ -5,7 +5,7 @@ from langchain_core.documents import Document
 
 load_dotenv()
 
-# Documents
+
 docs = [
     Document(page_content="Python is widely used in Artificial Intelligence.", metadata={"source": "AI_book"}),
     Document(page_content="Pandas is used for data analysis in Python.", metadata={"source": "DataScience_book"}),
