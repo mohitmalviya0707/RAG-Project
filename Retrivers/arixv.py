854f9207ce788462c1e9a5373f6f894fb3393612
Mohit Malviya <mohitmalviya0107@gmail.com>
2026-03-29 19:52:04 +0530

    Update arixv.py

--- Retrivers/arixv.py
+++ Retrivers/arixv.py
@@ -12,6 +12,6 @@ docs = retriever.invoke("large language models")
 
 for i, doc in enumerate(docs):
     print(f"\nResult {i+1}")
-    print("Title:", doc.metadata.get("Title"))
+    print("Title:", doc.metadata.get("Title")) 
     print("Authors:", doc.metadata.get("Authors"))
     print("Summary:", doc.page_content)  #  first 500 characters
